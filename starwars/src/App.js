55786509c3cf97e6574bbd34f0ce706a252318b6
slroberts <shomariroberts@gmail.comclear>
2020-05-31 23:25:34 -0400

    Completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import "./App.css";
+
+import axios from "axios";
+import Character from "./components/Character";
+import {Container, Row, Col} from "reactstrap";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.py4e.com/api/people/")
+      .then((response) => {
+        setCharacters(response.data.results);
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.log("Bruh!", error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Container>
+        <Row>
+          <Col sm="12" md={{size: 6, offset: 3}}>
+            {" "}
+            {characters.map((character) => {
+              return (
+                <Character
+                  key={character.name}
+                  name={character.name}
+                  gender={character.gender}
+                  birthYear={character.birth_year}
+                  eyeColor={character.eye_color}
+                  hairColor={character.hair_color}
+                  height={character.height}
+                  mass={character.mass}
+                />
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,67 @@
 // Write your Character component here
+import React, {useState} from "react";
+import {
+  Collapse,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from "reactstrap";
+import styled from "styled-components";
+
+const Character = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  const Characteristics = styled.ul`
+    list-style: none;
+    margin-left: -2.4rem;
+  `;
+
+  return (
+    <Card
+      body
+      key={props.name}
+      className="text-left"
+      style={{
+        marginBottom: "2rem",
+        opacity: ".85",
+        textTransform: "capitalize",
+      }}
+    >
+      <CardBody>
+        <CardTitle className="text-center">
+          <h2>{props.name}</h2>
+        </CardTitle>
+        <CardSubtitle
+          style={{marginBottom: "1rem"}}
+          className="text-center"
+          onClick={() => toggle()}
+        >
+          <Button color="warning">See Characteristics</Button>{" "}
+        </CardSubtitle>
+        {/* <CardText> */}
+        <Characteristics>
+          <Collapse isOpen={isOpen}>
+            <ListGroup>
+              <ListGroupItem>Gender: {props.gender}</ListGroupItem>
+              <ListGroupItem>Birth Year: {props.birthYear}</ListGroupItem>
+              <ListGroupItem>Eye Color: {props.eyeColor}</ListGroupItem>
+              <ListGroupItem>Hair Color: {props.hairColor}</ListGroupItem>
+              <ListGroupItem>Height: {props.height}</ListGroupItem>
+              <ListGroupItem>Mass: {props.mass}</ListGroupItem>
+            </ListGroup>
+          </Collapse>
+        </Characteristics>
+        {/* </CardText> */}
+      </CardBody>
+    </Card>
+  );
+};
+
+export default Character;
